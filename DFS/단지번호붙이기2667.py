a5648f99a009af5ce1353b7f0a4fd71671c4588b
zsert1 <zsert1@naver.com>
2023-11-22 09:30:54 -0500

    기초DFS

--- DFS/단지번호붙이기2667.py
+++ DFS/단지번호붙이기2667.py
@@ -2,11 +2,13 @@ N=int(input())
 graph=[]
 for i in range(N):
     graph.append(list(map(int,input())))
-zips=dict()
+num=[]
 def dfs(x,y):
      if x <= -1 or x >= N or y <= -1 or y >= N:
         return False
      if graph[x][y]==1:
+        global count
+        count+=1
         graph[x][y]=0
         dfs(x-1, y)
         dfs(x, y-1)
@@ -15,20 +17,22 @@ def dfs(x,y):
         return True
      
      return False
+count=0
 result = 0
 for i in range(N):
     for j in range(N):
         if dfs(i, j) == True:
+            num.append(count)
             result += 1
-            if(result not in zips):
-                zips[result]=0
-        else:
-            if(result in zips):
-                 zips[result]=zips[result]+1
+            count=0
+          
            
-        
-                
-        
-         
+num.sort()       
 print(result)
-print(zips)
+for i in num:
+    print(i)
+
+# 놓친 것 집의 갯수를 세는 count변수와 각 단지별 집의 갯수를 담아 줄 num리스트를 생각하지 못함
+# result에 +1를 하기전 count를 num리스트에 담고 그 후 count초기화 
+# num sort를 이용하여 정렬
+# for문 돌면서 num print하면 해결
